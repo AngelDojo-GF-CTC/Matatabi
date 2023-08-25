81179ae555401b8d49099878a5ea94fa18a857a6
sakurai <sakurai@geekfeed.co.jp>
2023-08-25 16:00:19 +0900

    amplifyコグニート設定

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "matatabicf3192bacf3192ba": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
