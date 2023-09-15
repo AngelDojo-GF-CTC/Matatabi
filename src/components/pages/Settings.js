74e94b319a6c01ba2b708491cbb25d0630a9f0ae
KondoRyuta <kondoujoho716@gmail.com>
2023-09-15 12:03:45 +0900

    settings.jp app.js試してみる

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -2,12 +2,6 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Layout } from "../templates/Layout";
 import { SignOutButton } from "../atoms/Buttons/SignOutButton";
-import { createMap } from "maplibre-gl-js-amplify";
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import "maplibre-gl/dist/maplibre-gl.css";
-
-Amplify.configure(awsconfig);
 
 export const Settings = () => {
   return (
