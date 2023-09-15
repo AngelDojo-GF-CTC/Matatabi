a19186feda57323f5e36204ad1c957778cfd938a
Wang <zichun.wang65@gmail.com>
2023-09-15 16:18:53 +0900

    変更前に戻す

--- App.js
+++ App.js
@@ -7,8 +7,7 @@ import { translations } from "@aws-amplify/ui";
 import { dict } from "./src/constants/lacales/auth";
 import { TabContainer } from "./src/navigations/TabContainer";
 import awsconfig from "./src/aws-exports";
-import { useEffect } from 'react';
-import { createMap } from "maplibre-gl-js-amplify";
+
 
 I18n.putVocabularies(translations);
 I18n.setLanguage("ja");
@@ -16,24 +15,14 @@ I18n.putVocabularies({ ...dict });
 
 Amplify.configure(awsconfig);
 
-function App() {
-  useEffect(() => {
-    createMap({
-      container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
-      center: [139.7674681227469, 35.68111419325676], // 東京駅
-      zoom: 14,
-  })
-  }, []);
+export default function App() {
   return (
-    <div id="map" style={{height: '100vh'}}/>
- //   <NativeBaseProvider>
- //     <Authenticater>
- //       <NavigationContainer>
- //         <TabContainer />
-  //      </NavigationContainer>
- //     </Authenticater>
- //   </NativeBaseProvider>
+   <NativeBaseProvider>
+     <Authenticater>
+       <NavigationContainer>
+         <TabContainer />
+       </NavigationContainer>
+     </Authenticater>
+   </NativeBaseProvider>
   );
 }
-
-export default App;
--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -2,21 +2,14 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Layout } from "../templates/Layout";
 import { SignOutButton } from "../atoms/Buttons/SignOutButton";
-import App from "../../../App";
 
-
-function App() {
-}
-
-export default App;
-
-//export const Settings = () => {
-//  return (
-//    <Layout>
-//      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//        <Text>Settings Screen</Text>
-//      </View>
-//      <SignOutButton />
-//    </Layout>
-//  );
-//};
+export const Settings = () => {
+ return (
+   <Layout>
+     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+       <Text>Settings Screen</Text>
+     </View>
+     <SignOutButton />
+   </Layout>
+ );
+};
