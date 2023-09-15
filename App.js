b3d2b10581e59d46441bfb08742fa9c86885f8d8
KondoRyuta <kondoujoho716@gmail.com>
2023-09-15 12:43:29 +0900

    app.js編集

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { translations } from "@aws-amplify/ui";
 import { dict } from "./src/constants/lacales/auth";
 import { TabContainer } from "./src/navigations/TabContainer";
 import awsconfig from "./src/aws-exports";
+import { useEffect } from 'react';
+import { createMap } from "maplibre-gl-js-amplify";
 
 I18n.putVocabularies(translations);
 I18n.setLanguage("ja");
@@ -14,14 +16,24 @@ I18n.putVocabularies({ ...dict });
 
 Amplify.configure(awsconfig);
 
-export default function App() {
+function App() {
+  useEffect(() => {
+    createMap({
+      container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
+      center: [139.7674681227469, 35.68111419325676], // 東京駅
+      zoom: 14,
+  })
+  }, []);
   return (
-    <NativeBaseProvider>
-      <Authenticater>
-        <NavigationContainer>
-          <TabContainer />
-        </NavigationContainer>
-      </Authenticater>
-    </NativeBaseProvider>
+    <div id="map" style={{height: '100vh'}}/>
+ //   <NativeBaseProvider>
+ //     <Authenticater>
+ //       <NavigationContainer>
+ //         <TabContainer />
+  //      </NavigationContainer>
+ //     </Authenticater>
+ //   </NativeBaseProvider>
   );
 }
+
+export default App;
--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -4,19 +4,7 @@ import { Layout } from "../templates/Layout";
 import { SignOutButton } from "../atoms/Buttons/SignOutButton";
 
 
-Amplify.configure(awsconfig);
-
 function App() {
-  useEffect(() => {
-    createMap({
-      container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
-      center: [139.7674681227469, 35.68111419325676], // 東京駅
-      zoom: 14,
-  })
-  }, []);
-  return (
-    <div id="map" style={{height: '100vh'}}/>
-  );
 }
 
 export default App;
