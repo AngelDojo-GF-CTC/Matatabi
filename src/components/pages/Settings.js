76b77bb6f5cc05e5123fc50427d61a01a55b057c
KondoRyuta <kondoujoho716@gmail.com>
2023-09-15 12:10:02 +0900

    error01

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -2,18 +2,34 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Layout } from "../templates/Layout";
 import { SignOutButton } from "../atoms/Buttons/SignOutButton";
+import { createMap } from "maplibre-gl-js-amplify";
+import awsconfig from "../../aws-exports";
+import "maplibre-gl/dist/maplibre-gl.css";
 
-export const Settings = () => {
+Amplify.configure(awsconfig);
+
+function App() {
+  useEffect(() => {
+    createMap({
+      container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
+      center: [139.7674681227469, 35.68111419325676], // 東京駅
+      zoom: 14,
+  })
+  }, []);
   return (
-    <Layout>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Settings Screen</Text>
-
-        
-
-
-      </View>
-      <SignOutButton />
-    </Layout>
+    <div id="map" style={{height: '100vh'}}/>
   );
-};
+}
+
+export default App;
+
+//export const Settings = () => {
+//  return (
+//    <Layout>
+//      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//        <Text>Settings Screen</Text>
+//      </View>
+//      <SignOutButton />
+//    </Layout>
+//  );
+//};
