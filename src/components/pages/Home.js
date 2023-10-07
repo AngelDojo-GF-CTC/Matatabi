72e0fb6e8aac6a58589141512e4f8f05f0e363ee
daikimatsuura <matsura@geekfeed.co.jp>
2023-10-07 23:27:11 +0900

    fix: TODOコメント削除

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -43,7 +43,6 @@ export const Home = () => {
       {pageMode.addFormMode ? (
         <TravelAddForm handleResetPage={handleResetPage} />
       ) : pageMode.travelDetailMode ? (
-        // TODO: 個別の旅のしおり閲覧ページ
         <Itinerary
           {...{
             travelName,
